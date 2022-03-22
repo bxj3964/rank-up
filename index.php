2ac4d9e06129664761c8a8819240972e177dad4d
Blake Juszkiewicz <bxj3964@rit.edu>
2022-03-21 21:56:19 -0400

    Updated links

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 				<img id="logo" src="assets/media/Gekkogamijin2.0_hollow.png">
 				<h1 id="title">Rank Up!</h1>
 			</div>
-			<a href="persona-3/index.html" id="p3">
+			<a href="persona-3/index.php" id="p3">
 				<img src="assets/media/p3_alt.png" alt="Persona 3 Logo" onmouseover="this.src='media/p3_alt_glow.png';" onmouseout="this.src='media/p3_alt.png';">
 			</a>
-			<a href="persona-4/index.html">
+			<a href="persona-4/index.php">
 				<img id="p4" src="assets/media/p4_alt.png" alt="Persona 4 Logo" onmouseover="this.src='media/p4_alt_glow.png';" onmouseout="this.src='media/p4_alt.png';">
 			</a>
-			<a href="persona-5/index.html">
+			<a href="persona-5/index.php">
 				<img id="p5" src="assets/media/p5_alt.png" alt="Persona 5 Logo" onmouseover="this.src='media/p5_alt_glow.png';" onmouseout="this.src='media/p5_alt.png';">
 			</a>
 			<div id="bottom">
