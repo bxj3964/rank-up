e92ed874ada74b44813eb4bc0998214839e58c7f
Blake Juszkiewicz <bxj3964@rit.edu>
2022-03-24 17:15:21 -0400

    Removed extra CSS and Media folder

--- persona-3/index.php
+++ persona-3/index.php
@@ -6,10 +6,10 @@
 	include $path.'assets/inc/header.php';
 ?>
 			<a id="p3">
-				<img src="<?php echo $path?>media/Persona_3_FES_logo.png" alt="Persona 3 FES Logo" onmouseover="this.src='<?php echo $path?>media/p4_alt_glow.png';" onmouseout="this.src='<?php echo $path?>media/p4_alt.png';">
+				<img src="<?php echo $path?>assets/media/Persona_3_FES_logo.png" alt="Persona 3 FES Logo" onmouseover="this.src='<?php echo $path?>media/p4_alt_glow.png';" onmouseout="this.src='<?php echo $path?>media/p4_alt.png';">
 			</a>
 			<a id="p3p">
-				<img src="<?php echo $path?>media/p5_alt.png" alt="Persona 5 Logo" onmouseover="this.src='<?php echo $path?>media/p5_alt_glow.png';" onmouseout="this.src='<?php echo $path?>media/p5_alt.png';">
+				<img src="<?php echo $path?>assets/media/p5_alt.png" alt="Persona 5 Logo" onmouseover="this.src='<?php echo $path?>media/p5_alt_glow.png';" onmouseout="this.src='<?php echo $path?>media/p5_alt.png';">
 			</a>
 			<div id="bottom">
 				<h1>Thou hast established a new bond...</h1>
