95d12729724dc2ef2b86bb181cb59d20a88eae6e
Blake Juszkiewicz <bxj3964@rit.edu>
2022-03-24 18:03:11 -0400

    Update image

--- persona-3/index.php
+++ persona-3/index.php
@@ -6,7 +6,7 @@
 	include $path.'assets/inc/header.php';
 ?>
 			<a id="p3">
-				<img src="<?php echo $path?>assets/media/Persona_3_FES_logo.png" alt="Persona 3 FES Logo" onmouseover="this.src='<?php echo $path?>media/p4_alt_glow.png';" onmouseout="this.src='<?php echo $path?>media/p4_alt.png';">
+				<img src="<?php echo $path?>assets/media/Persona_3_FES_logo.png" alt="Persona 3 FES Logo" onmouseover="this.src='<?php echo $path?>assets/media/Persona_3_FES_logo_glow.png';" onmouseout="this.src='<?php echo $path?>assets/media/Persona_3_FES_logo.png';">
 			</a>
 			<a id="p3p">
 				<img src="<?php echo $path?>assets/media/p5_alt.png" alt="Persona 5 Logo" onmouseover="this.src='<?php echo $path?>media/p5_alt_glow.png';" onmouseout="this.src='<?php echo $path?>media/p5_alt.png';">
