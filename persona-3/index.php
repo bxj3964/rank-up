ec3055b5dc022cee9d269d18b440687144a1f9b6
Blake Juszkiewicz <bxj3964@rit.edu>
2022-03-24 18:24:14 -0400

    P3P logo

--- persona-3/index.php
+++ persona-3/index.php
@@ -9,7 +9,7 @@
 				<img src="<?php echo $path?>assets/media/Persona_3_FES_logo.png" alt="Persona 3 FES Logo" onmouseover="this.src='<?php echo $path?>assets/media/Persona_3_FES_logo_glow.png';" onmouseout="this.src='<?php echo $path?>assets/media/Persona_3_FES_logo.png';">
 			</a>
 			<a id="p3p">
-				<img src="<?php echo $path?>assets/media/p5_alt.png" alt="Persona 5 Logo" onmouseover="this.src='<?php echo $path?>media/p5_alt_glow.png';" onmouseout="this.src='<?php echo $path?>media/p5_alt.png';">
+				<img src="<?php echo $path?>assets/media/Persona_3_Portable.png" alt="Persona 5 Logo" onmouseover="this.src='<?php echo $path?>assets/media/Persona_3_Portable_glow.png';" onmouseout="this.src='<?php echo $path?>assets/media/Persona_3_Portable.png';">
 			</a>
 			<div id="bottom">
 				<h1>Thou hast established a new bond...</h1>
