8cef6a0471b681022ee74b71ae72fae602710783
Blake Juszkiewicz <bxj3964@rit.edu>
2022-03-24 15:43:31 -0400

    Update Persona 3 home

--- persona-3/index.php
+++ persona-3/index.php
@@ -6,10 +6,10 @@
 	include $path.'assets/inc/header.php';
 ?>
 			<a>
-				<img id="p4" src="<?php echo $path?>media/p4_alt.png" alt="Persona 4 Logo" onmouseover="this.src='<?php echo $path?>media/p4_alt_glow.png';" onmouseout="this.src='<?php echo $path?>media/p4_alt.png';">
+				<img id="p3" src="<?php echo $path?>media/p4_alt.png" alt="Persona 4 Logo" onmouseover="this.src='<?php echo $path?>media/p4_alt_glow.png';" onmouseout="this.src='<?php echo $path?>media/p4_alt.png';">
 			</a>
 			<a>
-				<img id="p5" src="<?php echo $path?>media/p5_alt.png" alt="Persona 5 Logo" onmouseover="this.src='<?php echo $path?>media/p5_alt_glow.png';" onmouseout="this.src='<?php echo $path?>media/p5_alt.png';">
+				<img id="p3p" src="<?php echo $path?>media/p5_alt.png" alt="Persona 5 Logo" onmouseover="this.src='<?php echo $path?>media/p5_alt_glow.png';" onmouseout="this.src='<?php echo $path?>media/p5_alt.png';">
 			</a>
 			<div id="bottom">
 				<h1>Thou hast established a new bond...</h1>
