a2c7de01d2fccc4400cb81b6d4c9a6c50272c345
Blake Juszkiewicz <bxj3964@rit.edu>
2022-03-24 20:32:58 -0400

    Attempt at dynamic persona home

--- persona.php
+++ persona.php
@@ -4,6 +4,7 @@
 	if($conn && !empty($_GET['game']) && in_array($_GET['game'], $games){
 		$result = pg_prepare($conn, "query", 'SELECT * FROM Page WHERE id = $1');
 		$result = pg_execute($conn, "query", array($_GET['game']));
+		$result = $result[0]
 	}
 	else{
 		echo 'Error fetching page';
@@ -15,7 +16,12 @@
 	$assets='';
 	include $path.'assets/inc/header.php';
 ?>
-			
+			<a id="base">
+				<img src="<?php echo $path?>assets/media/<?php echo $result[4]?>.png" alt="<?php echo $result[3]?>" onmouseover="this.src='<?php echo $path?>assets/media/<?php echo $result[4]?>_glow.png';" onmouseout="this.src='<?php echo $path?>assets/media/<?php echo $result[4]?>.png';">
+			</a>
+			<a id="deluxe">
+				<img src="<?php echo $path?>assets/media/<?php echo $result[6]?>.png" alt="<?php echo $result[5]?>" onmouseover="this.src='<?php echo $path?>assets/media/<?php echo $result[6]?>_glow.png';" onmouseout="this.src='<?php echo $path?>assets/media/<?php echo $result[6]?>.png';">
+			</a>
 			<div id="bottom">
 				<h1>Thou hast established a new bond...</h1>
 			</div>
