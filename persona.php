d18a67ef08f47acce6694f3e13cc926df8e32796
Blake Juszkiewicz <bxj3964@rit.edu>
2022-03-24 21:17:34 -0400

    Attempt at dynamic persona home

--- persona.php
+++ persona.php
@@ -2,9 +2,9 @@
 	$conn = pg_connect(getenv("DATABASE_URL")) or die('Could not connect: ' . pg_last_error());
 	$games = array("3", "4", "5");
 	if($conn && !empty($_GET['game']) && in_array($_GET['game'], $games)){
-		$result = pg_prepare($conn, "query", "SELECT * FROM public.'Page' WHERE id = $1;");
+		$result = pg_prepare($conn, "query", "SELECT * FROM Page WHERE id = $1") or die('Query Failed.');
 		$result = pg_execute($conn, "query", array($_GET['game']));
-		echo $result;
+		$result = pg_fetch_row($result);
 	}
 	else{
 		echo 'Error fetching page';
