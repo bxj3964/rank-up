6a7c794c838baa8aa4c7fc92bf36275d25adc777
Blake Juszkiewicz <bxj3964@rit.edu>
2022-03-24 20:24:32 -0400

    Attempt at dynamic persona home

--- persona.php
+++ persona.php
@@ -1,19 +1,21 @@
 <?php
 	$conn = pg_connect(getenv("DATABASE_URL"));
-	if($conn){
-		echo 'connected';
+	$games = array("3", "4", "5")
+	if($conn && !empty($_GET['game']) && in_array($_GET['game'], $games){
+		$result = pg_prepare($conn, "query", 'SELECT * FROM Page WHERE id = $1');
+		$result = pg_execute($conn, "query", array($_GET['game']));
 	}
 	$path='./';
 	$css='persona_home';
-	$background=$path.'assets/media/P3D-Stage-5.jpg';
+	$background=$path.'assets/media/'.$result[2];
 	$assets='';
 	include $path.'assets/inc/header.php';
 ?>
-			<a id="p3">
-				<img src="<?php echo $path?>assets/media/Persona_3_FES_logo.png" alt="Persona 3 FES Logo" onmouseover="this.src='<?php echo $path?>assets/media/Persona_3_FES_logo_glow.png';" onmouseout="this.src='<?php echo $path?>assets/media/Persona_3_FES_logo.png';">
+			<a id="base">
+				<img src="<?php echo $path?>assets/media/<?php echo $result[4]?>.png" alt="<?php echo $result[3]?>" onmouseover="this.src='<?php echo $path?>assets/media/<?php echo $result[4]?>_glow.png';" onmouseout="this.src='<?php echo $path?>assets/media/<?php echo $result[4]?>.png';">
 			</a>
-			<a id="p3p">
-				<img src="<?php echo $path?>assets/media/Persona_3_Portable.png" alt="Persona 5 Logo" onmouseover="this.src='<?php echo $path?>assets/media/Persona_3_Portable_glow.png';" onmouseout="this.src='<?php echo $path?>assets/media/Persona_3_Portable.png';">
+			<a id="deluxe">
+				<img src="<?php echo $path?>assets/media/<?php echo $result[6]?>.png" alt="<?php echo $result[5]?>" onmouseover="this.src='<?php echo $path?>assets/media/<?php echo $result[6]?>_glow.png';" onmouseout="this.src='<?php echo $path?>assets/media/<?php echo $result[6]?>.png';">
 			</a>
 			<div id="bottom">
 				<h1>Thou hast established a new bond...</h1>
